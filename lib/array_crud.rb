cff31f61a6ebbc6255ea17edaf6a7bf0ef860e23
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 19:58:45 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,36 +1,35 @@
 def create_an_empty_array
-  []  
+  []
 end
 
 def create_an_array
-  ["dog", "cat", "fish", "owl"]
+ ["jet", "tank", "train", "battle ship"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element
+ [] << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element)
+ [].unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift
+ array.shift
 end
 
-def retreive_element_from_index(array, index_number)
+def retrieve_element_from_index(array, index_number)
   array[index_number]
 end
 
-def retreive_first_element_from_array(array)
+def retrieve_first_element_from_array(array)
   array[0]
 end
 
-def retreive_last_element_from_array(array)
-  x = array.count
-  array[x-1]
+def retrieve_last_element_from_array(array)
+array[-1]
 end
